42ad4098c37773b834ddcc7277e76a153ee66c47
Kenn Vun Gan <gervasiusgan@gmail.com>
2021-07-07 18:43:23 +0900

    Added cards

---
+++ src/components/CardItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const CardItem = (props) => {
+  return (
+    <div>
+      <li className="cards-item">
+        <Link className="cards-item-link" to={props.path}>
+          <figure className="cards-item-pic-wrap" data-category={props.label}>
+            <img
+              className="cards-item-img"
+              alt="travel"
+              src={props.src}
+            />
+          </figure>
+          <div className="cards-item-info">
+            <h5 className="cards-item-text">{props.text}</h5>
+          </div>
+        </Link>
+      </li>
+    </div>
+  );
+};
+
+export default CardItem;
